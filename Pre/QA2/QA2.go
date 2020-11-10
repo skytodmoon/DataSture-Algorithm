4aad07235515c036aaf9c4c20c64c8e909503e8c
孙琦 <sunqi5@hisense.com>
2020-11-10 15:42:47 +0800

    update

--- Pre/QA2/QA2.go
+++ Pre/QA2/QA2.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	var a = []int{1, 3, 4, 1, 3, 2, 3, 5, 10, 11, 4, 3}
@@ -9,6 +12,8 @@ func main() {
 	var valMax = -1
 
 	var i, j int = 0, 0
+	//first method
+	start0 := time.Now().UnixNano()
 	for i = 0; i < len(a); i++ {
 		timeTmp = 0
 		for j = 0; j < len(a); j++ {
@@ -23,5 +28,32 @@ func main() {
 	}
 
 	fmt.Println(valMax, timeMax)
+	end0 := time.Now().UnixNano()
+	fmt.Println("Time cost0 is :", (end0-start0)/1000)
+
+	//second method
+	start1 := time.Now().UnixNano()
+	checkMap := make(map[int]int)
+	for i = 0; i < len(a); i++ {
+		if _, ok := checkMap[a[i]]; ok {
+			checkMap[a[i]]++
+		} else {
+			checkMap[a[i]] = 1
+		}
+	}
+
+	var targetIndex = 0
+	var targetValue = -1
+
+	for value := range checkMap {
+		if checkMap[value] > targetValue {
+			targetIndex = value
+			targetValue = checkMap[value]
+		}
+	}
+
+	fmt.Println(targetIndex, targetValue)
+	end1 := time.Now().UnixNano()
+	fmt.Println("Time cost1 is :", (end1-start1)/1000)
 
 }

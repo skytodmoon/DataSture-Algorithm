ca7c928e6a3bb470ce68f497276d59ab80c7833f
孙琦 <sunqi5@hisense.com>
2020-11-13 10:38:20 +0800

    update

--- Pre/QA6/QA6.go
+++ Pre/QA6/QA6.go
@@ -1,10 +1,36 @@
 package main
 
-func main() {
-	// inputArray := [5]int{1, 3, 5, 7, 8}
-	// min, max := 0, 0
-	// for i := 0; i < len(inputArray); i++ {
+import (
+	"fmt"
+)
 
-	// }
+func main() {
+	inputArray := []int{2, 1, 4, 5, 3}
+
+	min, max, minIndex, maxIndex, avg, total := 99, 0, 0, 0, 0, 0
+
+	for i := 0; i < len(inputArray); i++ {
+		if inputArray[i] < min {
+			min = inputArray[i]
+			minIndex = i
+		}
+	}
+	inputArray = append(inputArray[:minIndex], inputArray[minIndex+1:]...)
+	for i := 0; i < len(inputArray); i++ {
+		if inputArray[i] > max {
+			max = inputArray[i]
+			maxIndex = i
+		}
+	}
+	inputArray = append(inputArray[:maxIndex], inputArray[maxIndex+1:]...)
+
+	fmt.Println(inputArray)
+
+	for i := 0; i < len(inputArray); i++ {
+		total += inputArray[i]
+	}
+
+	avg = total / len(inputArray)
+	fmt.Println("avg:", avg)
 
 }

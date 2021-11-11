fdac197a89d929dc8ab8353132ce9323ffecbfb6
sunqi <sunqi@haier.com>
2021-11-11 16:19:40 +0800

    update

--- Pre/QA44/main.go
+++ Pre/QA44/main.go
@@ -1,9 +1,37 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 115)
+	rotate([]int{-1, -100, 3, 99}, 2)
 }
 
-func rotate(nums []int, k int) {
+//func rotate(nums []int, k int) {
+//	var temp int
+//	for i := 0; i < k%len(nums); i++ {
+//		for j := len(nums) - 1; j >= 0; j-- {
+//			if j == len(nums)-1 {
+//				temp = nums[len(nums)-1]
+//			}
+//			if j == 0 {
+//				nums[j] = temp
+//			} else {
+//				nums[j] = nums[j-1]
+//			}
+//		}
+//	}
+//	fmt.Println(nums)
+//}
 
+func rotate(nums []int, k int) {
+	var tempNums = make([]int, len(nums))
+	var length = len(nums)
+	for i := 0; i < length; i++ {
+		tempNums[i] = nums[i]
+	}
+	for i := 0; i < length; i++ {
+		nums[(i+k)%len(nums)] = tempNums[i]
+	}
+	fmt.Println(nums)
 }
